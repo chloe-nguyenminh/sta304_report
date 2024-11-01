08bece42c1db1a35d037a5ae4aceb3b0670ef11d
chloe-nguyenminh <chau.nguyenminh109@gmail.com>
2024-11-01 16:26:20 -0400

    add initial file

---
+++ sta304_project_script.R
@@ -0,0 +1,21 @@
+my_data <- read.csv("STA304 Group Project Dataset.csv", header=TRUE, sep=',')
+# Datapreprocessing
+# Remove column 'X'
+data_cleaned <- my_data[,!(names(my_data)=="X")]
+# Remove fake responses on row 3 and 12
+data_cleaned <- data_cleaned[-c(3, 12),]
+data_cleaned
+
+# Calculating sample size
+N = 200
+B = .25
+D = (B^2)/4
+sigma = sd(data_cleaned$academic_workload)
+n = ceiling((N * sigma^2) / ((N-1) * D + sigma^2))
+n
+
+# Select a random sample of n rows
+set.seed(1)
+random_indices <- sample(1:nrow(data_cleaned), n)
+sample_data <- data_cleaned[random_indices, ]
+sample_data
