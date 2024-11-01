eabb15c6057e5f2d1b1be4fcc02a61ba8cdff128
sw99723 <48198355+sw99723@users.noreply.github.com>
2024-11-01 17:24:42 -0400

    Update sta304_project_script.R

--- sta304_project_script.R
+++ sta304_project_script.R
@@ -20,9 +20,28 @@ random_indices <- sample(1:nrow(data_cleaned), n)
 sample_data <- data_cleaned[random_indices, ]
 sample_data
 
-# new changes to ANOVA
+# new changes to ANOVA between stress and academic workload
 sample_data$stress_numeric <- as.numeric(factor(sample_data$stress, 
                                                 levels = c("Never", "Sometimes", "Always")))
 
 anova_result <- aov(stress_numeric ~ factor(academic_workload), data = sample_data)
 summary(anova_result)
+
+# new changes to ANOVA between stress and hours_sleep
+sample_data$stress_numeric <- as.numeric(factor(sample_data$stress, 
+                                                levels = c("Never", "Sometimes", "Always")))
+
+sample_data$sleep_category <- cut(sample_data$hours_sleep, 
+                                  breaks = c(0, 6, 8, Inf),
+                                  labels = c("Less than 6", "6-8", "More than 8"))
+
+anova_result <- aov(stress_numeric ~ sleep_category, data = sample_data)
+summary(anova_result)
+
+# new changes to ANOVA between stress and missed social events
+sample_data$study_category <- cut(sample_data$hours_study, 
+                                  breaks = c(0, 10, 20, Inf),
+                                  labels = c("Low", "Medium", "High"))
+
+anova_social <- aov(stress_numeric ~ factor(missed_social_events), data = sample_data)
+print(summary(anova_social))
