13b3acf91282eac8808e62543c4eaece768a5e81
john <johnluc0718@gmail.com>
2024-11-11 12:23:08 -0500

    ANOVA discussion and results

--- sta304_project_script.R
+++ sta304_project_script.R
@@ -8,7 +8,7 @@ data_cleaned
 
 # Calculating sample size
 N = 200
-B = .25
+B = .29
 D = (B^2)/4
 sigma = sd(data_cleaned$academic_workload)
 n = ceiling((N * sigma^2) / ((N-1) * D + sigma^2))
@@ -19,3 +19,53 @@ set.seed(1)
 random_indices <- sample(1:nrow(data_cleaned), n)
 sample_data <- data_cleaned[random_indices, ]
 sample_data
+
+sample_data$stress_numeric <- as.numeric(factor(sample_data$stress, 
+                                                levels = c("Never", "Sometimes", "Always")))
+
+anova_result <- aov(stress_numeric ~ factor(academic_workload), data = sample_data)
+summary(anova_result)
+
+# Ensure that stress is a factor with the correct levels
+data_cleaned$stress <- factor(data_cleaned$stress, levels = c("Never", "Sometimes", "Always"))
+
+# Box plot
+boxplot(academic_workload ~ stress, data = data_cleaned, 
+        main = "Box Plot of Academic Workload by Stress Levels",
+        xlab = "Stress Level", 
+        ylab = "Academic Workload")
+
+# ANOVA between stress and hours_sleep
+sample_data$stress_numeric <- as.numeric(factor(sample_data$stress, 
+                                                levels = c("Never", "Sometimes", "Always")))
+
+sample_data$sleep_category <- cut(sample_data$hours_sleep, 
+                                  breaks = c(0, 6, 8, Inf),
+                                  labels = c("Less than 6", "6-8", "More than 8"))
+
+anova_result <- aov(stress_numeric ~ sleep_category, data = sample_data)
+summary(anova_result)
+
+sample_data$sleep_category <- factor(sample_data$sleep_category, levels = c("Less than 6", "6-8", "More than 8"))
+
+# Box plot 
+boxplot(stress_numeric ~ sleep_category, data = sample_data,
+        main = "Box Plot of Stress Levels by Hours of Sleep Category",
+        xlab = "Hours of Sleep Category",
+        ylab = "Stress Level (Numeric)")
+
+# ANOVA between stress and missed social events
+sample_data$study_category <- cut(sample_data$hours_study, 
+                                  breaks = c(0, 10, 20, Inf),
+                                  labels = c("Low", "Medium", "High"))
+
+anova_social <- aov(stress_numeric ~ factor(missed_social_events), data = sample_data)
+print(summary(anova_social))
+
+sample_data$missed_social_events <- factor(sample_data$missed_social_events)
+
+# Box plot 
+boxplot(stress_numeric ~ missed_social_events, data = sample_data,
+        main = "Box Plot of Stress Levels by Missed Social Events",
+        xlab = "Missed Social Events",
+        ylab = "Stress Level (Numeric)")
