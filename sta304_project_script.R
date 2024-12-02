90e3f886160ac3e1228c25661362976f4f84bd8c
john <johnluc0718@gmail.com>
2024-12-02 12:41:03 -0500

    replace anova with kuchis wallis

--- sta304_project_script.R
+++ sta304_project_script.R
@@ -14,38 +14,6 @@ sigma = sd(data_cleaned$academic_workload)
 n = ceiling((N * sigma^2) / ((N-1) * D + sigma^2))
 n
 
-# Select a random sample of n rows
-set.seed(1)
-random_indices <- sample(1:nrow(data_cleaned), n)
-sample_data <- data_cleaned[random_indices, ]
-sample_data
-
-# new changes to ANOVA between stress and academic workload
-sample_data$stress_numeric <- as.numeric(factor(sample_data$stress, 
-                                                levels = c("Never", "Sometimes", "Always")))
-
-anova_result <- aov(stress_numeric ~ factor(academic_workload), data = sample_data)
-summary(anova_result)
-
-# new changes to ANOVA between stress and hours_sleep
-sample_data$stress_numeric <- as.numeric(factor(sample_data$stress, 
-                                                levels = c("Never", "Sometimes", "Always")))
-
-sample_data$sleep_category <- cut(sample_data$hours_sleep, 
-                                  breaks = c(0, 6, 8, Inf),
-                                  labels = c("Less than 6", "6-8", "More than 8"))
-
-anova_result <- aov(stress_numeric ~ sleep_category, data = sample_data)
-summary(anova_result)
-
-# new changes to ANOVA between stress and missed social events
-sample_data$study_category <- cut(sample_data$hours_study, 
-                                  breaks = c(0, 10, 20, Inf),
-                                  labels = c("Low", "Medium", "High"))
-
-anova_social <- aov(stress_numeric ~ factor(missed_social_events), data = sample_data)
-print(summary(anova_social))
-
 my_data <- read.csv("STA304 Group Project Dataset.csv", header=TRUE, sep=',')
 # Datapreprocessing
 # Remove column 'X'
